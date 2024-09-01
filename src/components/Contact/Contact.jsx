c0a0b06ab32910badb1ecd657e490b118752a7ce
Yurii2807 <156546118+Yurii2807@users.noreply.github.com>
2024-09-01 12:48:14 +0200

    update file

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import { FaUserAlt } from "react-icons/fa";
 import { FaPhoneVolume } from "react-icons/fa6";
 import css from "./Contact.module.css";
 
-import { deleteContact } from "../../redux/contactsSlice";
+import { deleteContact } from "../../redux/contactsOps";
 import { useDispatch } from "react-redux";
 
 const Contact = ({ contact }) => {
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,34 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filtersSlice";
-import {
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-  persistStore,
-  persistReducer,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
 
-const contactsConfig = {
-  key: "contacts",
-  storage,
-  whitelist: ["items"],
-};
 export const store = configureStore({
   reducer: {
-    contacts: persistReducer(contactsConfig, contactsReducer),
+    contacts: contactsReducer,
     filters: filterReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
